f80df00da1f5740284456c1d4cd58f48e9811e7f
JordyAM <JAlexanderMcLean@Gmail.com>
2022-04-11 19:03:12 -0400

    building backend

---
+++ models/Comment.js
@@ -0,0 +1,6 @@
+const mongoose = require('../db/connection');
+const commentSchema = new mongoose.Schema({
+    message: String,
+})
+
+module.exports = commentSchema;
--- models/User.js
+++ models/User.js
@@ -1,7 +1,10 @@
 const mongoose = require("../db/connection");
+const commentSchema = require('')
 
 const userSchema = new mongoose.Schema({
     name: String,
     avatar: String,
-    comment: String,
+    comment: [commentSchema],
 })
+
+module.exports = userSchema;
