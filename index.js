428474bef4648b7609b8e48e9839b21c87387a6c
JordyAM <JAlexanderMcLean@Gmail.com>
2022-04-18 10:03:06 -0400

    backend updates

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const http = require('http').Server(app);
-// const socketio = require('socket.io');
+
+
 app.set("port", process.env.PORT || 8000);
 const io = require('socket.io')(http, {cors: {origin: '*'}})
 
@@ -13,18 +14,12 @@ app.use(cors());
 
 //When someone connects
 io.on('connection', socket => {
-    // console.log('New connection...');
 
     socket.on('message', ({name, message}) => {
-        console.log(message);
         io.emit('message', {name, message})
-        // io.emit('message', `${socket.id.substr(0,2)} said ${message}` );   
     });
 })
 
-// app.get("/", (req, res) => {
-//     // return res.redirect("/")
-// })
 
 const boxController = require('./controllers/boxController');
 app.use('/boxes', boxController);
